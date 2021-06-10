6a8ca08a1502027091c7ca68b735dd1767d5eec9
Richard Kooijman <rkooijman@quicknet.nl>
2021-06-10 17:17:15 +0200

    version 0.9

--- timify-heatmaps/timify-heatmaps.js
+++ timify-heatmaps/timify-heatmaps.js
@@ -168,7 +168,7 @@
     function initializeButtons() {
         $('#show_choose_sheet_button').click(showChooseSheetDialog);
         $('#reset_filters_button').click(resetFilters);
-        //$('#send_to_timify_button').click(sendToTimify);
+        $('#send_to_timify_button').click(sendToTimify);
     }
 
     // Save the columns we've applied filters to so we can reset them
@@ -210,193 +210,193 @@
         });
     }
 
-    // const timify_base_url = "https://api.timify.com/v1";
-    // const timify_auth_path = "auth/token";
+    const timify_base_url = "https://api.timify.com/v1";
+    const timify_auth_path = "auth/token";
 
-    // // app credentials
-    // const app_id = "6076ede9c017fc117103bfbd";
-    // const app_secret = "a4aa1715-b320-488e-a341-a10c9e240e50";
+    // app credentials
+    const app_id = "6076ede9c017fc117103bfbd";
+    const app_secret = "a4aa1715-b320-488e-a341-a10c9e240e50";
 
-    // // enterprise data
-    // const enterprise_id = "5fe088b075be2d11bd18a578"
+    // enterprise data
+    const enterprise_id = "5fe088b075be2d11bd18a578"
 
-    // const timify_auth_url = timify_base_url + "/" + timify_auth_path + "?" + "appid=" + app_id + "&appsecret=" + app_secret;
+    const timify_auth_url = timify_base_url + "/" + timify_auth_path + "?" + "appid=" + app_id + "&appsecret=" + app_secret;
 
-    // const getAccessToken = async () => {
-    //     try {
-    //         const response = await fetch(timify_auth_url);
-    //         const json = await response.json();
-    //         return json;
-    //     } catch (error) {
-    //         console.error("there has been a problem with the retrieval of the access token: ", error.message);
-    //         throw error;
-    //     }
-    // };
+    const getAccessToken = async () => {
+        try {
+            const response = await fetch(timify_auth_url);
+            const json = await response.json();
+            return json;
+        } catch (error) {
+            console.error("there has been a problem with the retrieval of the access token: ", error.message);
+            throw error;
+        }
+    };
 
-    // const headers = (method, accessToken) => {
-    //     return {
-    //         "method": method,
-    //         "headers": {
-    //             "Accept": "application/json",
-    //             "Content-Type": "application/json",
-    //             authorization: accessToken,
-    //         },
-    //     };
-    // };
+    const headers = (method, accessToken) => {
+        return {
+            "method": method,
+            "headers": {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "authorization": accessToken,
+            },
+        };
+    };
 
-    // const getCompanies = async (enterprise_id, accessToken) => {
-    //     // retrieve a list of companies for the specified enterprise id
-    //     try {
-    //         const timify_companies_path = "companies";
-    //         const timify_companies_url = timify_base_url + "/" + timify_companies_path + "?" + "enterprise_id=" + enterprise_id;
-    //         const options = headers("GET", accessToken);
-    //         const response = await fetch(timify_companies_url, options);
-    //         const json = await response.json();
-    //         return json;
-    //     } catch (error) {
-    //         console.error("there has been a problem with the retrieval of the companies information: ", error.message);
-    //         throw error;
-    //     }
-    // };
+    const getCompanies = async (enterprise_id, accessToken) => {
+        // retrieve a list of companies for the specified enterprise id
+        try {
+            const timify_companies_path = "companies";
+            const timify_companies_url = timify_base_url + "/" + timify_companies_path + "?" + "enterprise_id=" + enterprise_id;
+            const options = headers("GET", accessToken);
+            const response = await fetch(timify_companies_url, options);
+            const json = await response.json();
+            return json;
+        } catch (error) {
+            console.error("there has been a problem with the retrieval of the companies information: ", error.message);
+            throw error;
+        }
+    };
 
-    // const postFootfallMapping = async (id, accessToken, footfallMapping) => {
-    //     // send the footfall mapping for the store id to Timify
-    //     try {
-    //         const timify_companies_settings_path = `companies/${id}/settings`;
-    //         const timify_companies_settings_url = timify_base_url + "/" + timify_companies_settings_path;
-    //         let options = headers("POST", accessToken);
-    //         options.body = JSON.stringify({
-    //             "footfall_mapping": footfallMapping,
-    //         });
-    //         const response = await fetch(timify_companies_settings_url, options);
-    //         const json = await response.json();
-    //         return json;
-    //     } catch (error) {
-    //         console.error("there has been a problem with the posting of footfall mappings: ", error.message);
-    //         throw error;
-    //     }
-    // };
+    const postFootfallMapping = async (id, accessToken, footfallMapping) => {
+        // send the footfall mapping for the store id to Timify
+        try {
+            const timify_companies_settings_path = `companies/${id}/settings`;
+            const timify_companies_settings_url = timify_base_url + "/" + timify_companies_settings_path;
+            let options = headers("PUT", accessToken);
+            options.body = JSON.stringify({
+                "footfall_mapping": footfallMapping,
+            });
+            const response = await fetch(timify_companies_settings_url, options);
+            const json = await response.json();
+            return json;
+        } catch (error) {
+            console.error("there has been a problem with the posting of footfall mappings: ", error.message);
+            throw error;
+        }
+    };
 
-    // function padN(num, length) {
-    //     return String(num).padStart(length, "0");
-    // }
+    function padN(num, length) {
+        return String(num).padStart(length, "0");
+    }
 
-    // function convertToDwhConvention(externalId, enterpriseName) {
-    //     // create a HA standardized and unique name for E+M stores
-    //     switch (enterpriseName) {
-    //         case "eyes + more Österreich":
-    //             return "EA" + padN(externalId, 4);
-    //         case "eyes + more Deutschland":
-    //             return "EG" + padN(externalId, 4);
-    //         case "eyes + more België":
-    //             return "EB" + padN(externalId, 4);
-    //         case "eyes + more Nederland":
-    //             return "EN" + padN(externalId, 4);;
-    //         default:
-    //             return externalId;
-    //     }
-    // }
+    function convertToDwhConvention(externalId, enterpriseName) {
+        // create a HA standardized and unique name for E+M stores
+        switch (enterpriseName) {
+            case "eyes + more Österreich":
+                return "EA" + padN(externalId, 4);
+            case "eyes + more Deutschland":
+                return "EG" + padN(externalId, 4);
+            case "eyes + more België":
+                return "EB" + padN(externalId, 4);
+            case "eyes + more Nederland":
+                return "EN" + padN(externalId, 4);;
+            default:
+                return externalId;
+        }
+    }
 
-    // const lookupCompanyData = (data, columns, externalId, enterpriseName) => {
-    //     const store = convertToDwhConvention(externalId, enterpriseName);
-    //     columnIndexDay = columns.find(column => column.title.toUpperCase().indexOf("DAYOFWEEK"));
-    //     columnIndexHour = columns.find(column => column.title.toUpperCase().indexOf("HOUR"));
-    //     columnIndexHeatmap = columns.find(column => column.title.toUpperCase().matches("HEATMAP.*NAME"));
+    const lookupCompanyData = (data, columns, externalId, enterpriseName) => {
+        const store = convertToDwhConvention(externalId, enterpriseName);
+        let columnIndexDay = columns.findIndex(column => column.title.toUpperCase().indexOf("DAYOFWEEK") > -1);
+        let columnIndexHour = columns.findIndex(column => column.title.toUpperCase().indexOf("HOUR") > -1);
+        let columnIndexHeatmap = columns.findIndex(column => column.title.toUpperCase().match("HEATMAP.*NAME"));
 
-    //     // create empty intervals for the store
-    //     let intervals = {
-    //         "found": false,
-    //         "monday": [],
-    //         "tuesday": [],
-    //         "wednesday": [],
-    //         "thursday": [],
-    //         "friday": [],
-    //         "saturday": [],
-    //         "sunday": []
-    //     };
+        // create empty intervals for the store
+        let intervals = {
+            "found": false,
+            "monday": [],
+            "tuesday": [],
+            "wednesday": [],
+            "thursday": [],
+            "friday": [],
+            "saturday": [],
+            "sunday": []
+        };
 
-    //     if (store in data) {
-    //         // go through the data rows of a store
-    //         data[store].rows.forEach((row, index) => {
-    //             const day = row[columnIndexDay];
-    //             const hour = row[columnIndexHour];
-    //             const heatmap = row[columnIndexHeatmap];
-    //             // create an interval record
-    //             const intervalValue = { "begin": padN(hour, 2), "end": padN(hour + 1, 2), "footfall": heatmap };
-    //             // depending on the day of the week, append the interval record to the right day array
-    //             switch (parseInt(day)) {
-    //                 case 1:
-    //                     intervals.monday.push(intervalValue);
-    //                     break;
-    //                 case 2:
-    //                     intervals.tuesday.push(intervalValue);
-    //                     break;
-    //                 case 3:
-    //                     intervals.wednesday.push(intervalValue);
-    //                     break;
-    //                 case 4:
-    //                     intervals.thursday.push(intervalValue);
-    //                     break;
-    //                 case 5:
-    //                     intervals.friday.push(intervalValue);
-    //                     break;
-    //                 case 6:
-    //                     intervals.saturday.push(intervalValue);
-    //                     break;
-    //                 case 7:
-    //                     intervals.sunday.push(intervalValue);
-    //                     break;
-    //                 default:
-    //                     throw new Error("invalid day of week received in data");
-    //             }
-    //         });
-    //         // signal we actually found a store
-    //         intervals.found = true;
-    //     }
-    //     return intervals;
-    // };
+        if (store in data) {
+            // go through the data rows of a store
+            data[store].rows.forEach((row, index) => {
+                const day = row[columnIndexDay];
+                const hour = row[columnIndexHour];
+                const heatmap = row[columnIndexHeatmap];
+                // create an interval record
+                const intervalValue = { "begin": padN(hour, 2)+":00", "end": padN(parseInt(hour) + 1, 2)+":00", "footfall": heatmap };
+                // depending on the day of the week, append the interval record to the right day array
+                switch (parseInt(day)) {
+                    case 1:
+                        intervals.monday.push(intervalValue);
+                        break;
+                    case 2:
+                        intervals.tuesday.push(intervalValue);
+                        break;
+                    case 3:
+                        intervals.wednesday.push(intervalValue);
+                        break;
+                    case 4:
+                        intervals.thursday.push(intervalValue);
+                        break;
+                    case 5:
+                        intervals.friday.push(intervalValue);
+                        break;
+                    case 6:
+                        intervals.saturday.push(intervalValue);
+                        break;
+                    case 7:
+                        intervals.sunday.push(intervalValue);
+                        break;
+                    default:
+                        throw new Error("invalid day of week received in data");
+                }
+            });
+            // signal we actually found a store
+            intervals.found = true;
+        }
+        return intervals;
+    };
 
-    // let storeHash = {};
+    function convertDataToStoreHash(data, columns) {
+        let columnIndexStore = columns.findIndex(column => column.title.toUpperCase().startsWith("STORE"));
+        let storeHash = {};
+        data.forEach((row, index) => {
+            const store = row[columnIndexStore];
+            if (!(store in storeHash)) {
+                storeHash[store] = { "rows": [] };
+            }
+            storeHash[store].rows.push(row);
+        });
+        return storeHash;
+    }
 
-    // function convertDataToStoreHash(data, columns) {
-    //     columnIndexStore = columns.find(column => column.title.toUpperCase().startsWith("STORE"));
-    //     storeHash = {};
-    //     data.forEach((row, index) => {
-    //         const store = row[columnIndexStore];
-    //         if (!(store in storeHash)) {
-    //             storeHash[store] = { "rows": [] };
-    //         }
-    //         storeHash[store].rows.push(row);
-    //     });
-    // }
-
-    // function sendToTimify(data, columns) {
-    //     const storeData = convertDataToStoreHash(data, columns);
-    //     getAccessToken(timify_auth_url).then((response) => {
-    //         console.debug(response);
-    //         const accessToken = response.accessToken;
-    //         getCompanies(enterprise_id, accessToken).then((response) => {
-    //             console.debug(response);
-    //             response.data.forEach((company, index, arrays) => {
-    //                 intervals = lookupCompanyData(storeData, columns, company.externalId, company.enterprise.name);
-    //                 // get footfallMapping for company external id
-    //                 // intervals: [{ "footfall": "GREEN", "begin": "09:00", "end": "17:00" }]
-    //                 if (intervals.found) {
-    //                     footfallMapping = [
-    //                             { "isActive": (intervals.monday.lenght == 0 ?    false : true), "intervals": intervals.monday },
-    //                             { "isActive": (intervals.tuesday.lenght == 0 ?   false : true), "intervals": intervals.tuesday },
-    //                             { "isActive": (intervals.wednesday.lenght == 0 ? false : true), "intervals": intervals.wednesday },
-    //                             { "isActive": (intervals.thursday.lenght == 0 ?  false : true), "intervals": intervals.thursday },
-    //                             { "isActive": (intervals.friday.lenght == 0 ?    false : true), "intervals": intervals.friday },
-    //                             { "isActive": (intervals.saturday.lenght == 0 ?  false : true), "intervals": intervals.saturday },
-    //                             { "isActive": (intervals.sunday.lenght == 0 ?    false : true), "intervals": intervals.sunday }
-    //                     ];
-    //                     postFootfallMapping(company.id, accessToken, footfallMapping).then((response) => {
-    //                         console.log(response);
-    //                     });                
-    //                 }
-    //             });
-    //         });
-    //     });   
-    // }
+    // called from click on send_to_timify_button
+    function sendToTimify() {
+        const storeData = convertDataToStoreHash(data, columns);
+        getAccessToken(timify_auth_url).then((response) => {
+            console.debug(response);
+            const accessToken = response.accessToken;
+            getCompanies(enterprise_id, accessToken).then((response) => {
+                console.debug(response);
+                response.data.forEach((company, index, arrays) => {
+                    let intervals = lookupCompanyData(storeData, columns, company.externalId, company.enterprise.name);
+                    // get footfallMapping for company external id
+                    // intervals: [{ "footfall": "GREEN", "begin": "09:00", "end": "17:00" }]
+                    if (intervals.found) {
+                        const footfallMapping = [
+                                { "isActive": (intervals.monday.lenght == 0 ?    false : true), "intervals": intervals.monday },
+                                { "isActive": (intervals.tuesday.lenght == 0 ?   false : true), "intervals": intervals.tuesday },
+                                { "isActive": (intervals.wednesday.lenght == 0 ? false : true), "intervals": intervals.wednesday },
+                                { "isActive": (intervals.thursday.lenght == 0 ?  false : true), "intervals": intervals.thursday },
+                                { "isActive": (intervals.friday.lenght == 0 ?    false : true), "intervals": intervals.friday },
+                                { "isActive": (intervals.saturday.lenght == 0 ?  false : true), "intervals": intervals.saturday },
+                                { "isActive": (intervals.sunday.lenght == 0 ?    false : true), "intervals": intervals.sunday }
+                        ];
+                        postFootfallMapping(company.id, accessToken, footfallMapping).then((response) => {
+                            console.log(response);
+                        });                
+                    }
+                });
+            });
+        });   
+    }
 })();
